a5cef7c725bfc0a97434004f448afa98b213ef33
ViVoTm <ajaghvajaghemail@gmail.com>
2016-03-11 01:39:28 +0330

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -1,5 +1,5 @@
 do
---edited by ViVo™ ID CHANNEL : @ViVo_Team
+--create by RoyalTeam ID CHANNEL : @RoyalTeamCh
 -- Returns the key (index) in the config.enabled_plugins table
 local function plugin_enabled( name )
   for k,v in pairs(_config.enabled_plugins) do
@@ -24,15 +24,15 @@ end
 local function list_plugins(only_enabled)
   local text = ''
   for k, v in pairs( plugins_names( )) do
-    --  ■ enabled, □ disabled
-    local status = '□'
+    --  ★ enabled, ☆ disabled
+    local status = '☆'
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '■' 
+        status = '★' 
       end
     end
-    if not only_enabled or status == '■' then
+    if not only_enabled or status == '★' then
       -- get the name
       v = string.match (v, "(.*)%.lua")
       text = text..v..'  '..status..'\n'
@@ -122,12 +122,12 @@ end
 
 local function run(msg, matches)
   -- Show the available plugins 
-  if matches[1] == '/plugins' then
+  if matches[1] == '/pugins' then
     return list_plugins()
   end
 
   -- Re-enable a plugin for this chat
-  if matches[1] == '+pl' and matches[3] == 'supergroup' then
+  if matches[1] == '+' and matches[3] == 'supergroup' then
     local receiver = get_receiver(msg)
     local plugin = matches[2]
     print("enable "..plugin..' on this supergroup')
@@ -135,53 +135,50 @@ local function run(msg, matches)
   end
 
   -- Enable a plugin
-  if matches[1] == '+pl' then
+  if matches[1] == '+' then
     local plugin_name = matches[2]
     print("enable: "..matches[2])
     return enable_plugin(plugin_name)
   end
 
   -- Disable a plugin on a chat
-  if matches[1] == '-pl' and matches[3] == 'supergroup' then
+  if matches[1] == '-' and matches[3] == 'supergroup' then
     local plugin = matches[2]
     local receiver = get_receiver(msg)
-    print("disable "..plugin..' on this chat')
+    print("disable "..plugin..' on this supergroup')
     return disable_plugin_on_chat(receiver, plugin)
   end
 
   -- Disable a plugin
-  if matches[1] == '-pl' then
+  if matches[1] == '-' then
     print("disable: "..matches[2])
     return disable_plugin(matches[2])
   end
 
   -- Reload all the plugins!
-  if matches[1] == '↻' then
+  if matches[1] == '*' then
     return reload_plugins(true)
   end
 end
---edited by ViVo ™ ID CHANNEL : @ViVo_Team
+--create by RoyalTeam ID CHANNEL : @RoyalTeamCh
 return {
   description = "Plugin to manage other plugins. Enable, disable or reload.", 
   usage = {
     "!plugins: list all plugins.", 
-    "+pl [plugin]: enable plugin.",
-    "-pl [plugin]: disable plugin.",
-    "-pl [plugin] chat: disable plugin only this chat.",
+    "!plugins enable [plugin]: enable plugin.",
+    "!plugins disable [plugin]: disable plugin.",
+    "!plugins disable [plugin] chat: disable plugin only this chat.",
     "!plugins reload: reloads all plugins." },
   patterns = {
-    "^[/!#]plugins$",
-    "^+pl? ([%w_%.%-]+)$",
-    "^-pl? ([%w_%.%-]+)$",
-    "^-pl? ([%w_%.%-]+) (chat)$",
-    "^+pl? ([%w_%.%-]+) (supergroup)",
-    "^-pl? ([%w_%.%-]+) (supergroup)",
-    "^[/!#]plugins? (↻)$" },
+    "^[!/]plugins$",
+    "^[!/]plugins? (+) ([%w_%.%-]+)$",
+    "^[!/]plugins? (-) ([%w_%.%-]+)$",
+    "^[!/]plugins? (-) ([%w_%.%-]+) (chat)$",
+    "^[!/]plugins? (+) ([%w_%.%-]+) (supergroup)",
+    "^[!/]plugins? (-) ([%w_%.%-]+) (supergroup)",
+    "^[!/]plugins? (*)$" },
   run = run,
   privileged = true
 }
 
 end
-
--- کپی بدون ذکر نام ادیتور شرعا حرام است
--- @ViVo_Team
