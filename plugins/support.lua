efc840f17026e873c967ac2af2bbe518402db834
ViVoTm <ajaghvajaghemail@gmail.com>
2016-03-16 15:31:56 +0330

    Create support.lua

---
+++ plugins/support.lua
@@ -0,0 +1,35 @@
+do
+
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg, matches)
+  local user = 120816252
+
+  if matches[1] == "support" then
+    user = 'user#id'..user
+  end
+
+  -- The message must come from a chat group
+  if msg.to.type == 'chat' or 
+  if msg.to.type == 'channel' then
+    local chat = 'channe#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    return "سازنده ربات وارد گروه شد"
+  else 
+    return 'This isnt a chat group!'
+  end
+
+end
+
+return {
+  description = "support", 
+  patterns = {
+    "^[!/#](support)$"
+  }, 
+  run = run 
+}
+
+end
