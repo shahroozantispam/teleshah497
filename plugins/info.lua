63b07149f4717538422e9ff94c107a0322a5c76a
ViVoTm <ajaghvajaghemail@gmail.com>
2016-03-16 13:51:30 +0330

    Create info.lua

---
+++ plugins/info.lua
@@ -0,0 +1,13 @@
+do
+function run(msg, matches)
+  return "Group Id : "..msg.from.id.."\nGroup Name : "..msg.to.title.."\nFull Name : "..(msg.from.first_name or '').."\nFirst Name : "..(msg.from.first_name or '').."\nLast Name : "..(msg.from.last_name or '').."\nYour Id : "..msg.from.id.."\nYour Username : @"..(msg.from.username or '').."\nPhone Number : +"..(msg.from.phone or '')
+end
+return {
+  description = "", 
+  usage = "",
+  patterns = {
+    "^[!/#][Ii]nfo$",
+  },
+  run = run
+}
+end
